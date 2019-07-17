85849956beb1dc893afd95d33b61e35194d532a7
xujiajun <me@xujiajun.cn>
2019-07-17 17:11:39 +0800

    Fix error too many open files in system

--- rwmanger_mmap.go
+++ rwmanger_mmap.go
@@ -1,10 +1,24 @@
+// Copyright 2019 The nutsdb Author. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package nutsdb
 
 import (
 	"errors"
 	"os"
 
-	"github.com/xujiajun/mmap-go"
+	mmap "github.com/xujiajun/mmap-go"
 )
 
 // MMapRWManager represents the RWManager which using mmap.
@@ -23,6 +37,8 @@ var (
 // NewMMapRWManager returns a newly initialized MMapRWManager.
 func NewMMapRWManager(path string, capacity int64) (*MMapRWManager, error) {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0644)
+	defer f.Close()
+
 	if err != nil {
 		return nil, err
 	}
