5703d5dc7b812bb4d16bf2e35c82d9e3ea3b9b24
keyu <957681484@qq.com>
2022-11-13 21:38:44 +0800

    refactor:use ReadFull() instead of Read()

--- recovery_reader.go
+++ recovery_reader.go
@@ -3,6 +3,7 @@ package nutsdb
 import (
 	"bufio"
 	"encoding/binary"
+	"io"
 	"os"
 )
 
@@ -23,7 +24,8 @@ func newFileRecovery(path string) (fr *fileRecovery, err error) {
 
 // readEntry will read a Entry from disk.
 func (fr *fileRecovery) readEntry() (e *Entry, err error) {
-	buf, err := fr.readData(DataEntryHeaderSize)
+	buf := make([]byte, DataEntryHeaderSize)
+	_, err = io.ReadFull(fr.reader, buf)
 	if err != nil {
 		return nil, err
 	}
@@ -39,8 +41,8 @@ func (fr *fileRecovery) readEntry() (e *Entry, err error) {
 	}
 
 	dataSize := meta.BucketSize + meta.KeySize + meta.ValueSize
-
-	dataBuf, err := fr.readData(dataSize)
+	dataBuf := make([]byte, dataSize)
+	_, err = io.ReadFull(fr.reader, dataBuf)
 	if err != nil {
 		return nil, err
 	}
@@ -55,20 +57,4 @@ func (fr *fileRecovery) readEntry() (e *Entry, err error) {
 	}
 
 	return e, nil
-}
-
-// readData will read a byte array from disk by given size, and if the byte size less than given size in the first time it will read twice for the rest data.
-func (fr *fileRecovery) readData(size uint32) (data []byte, err error) {
-	data = make([]byte, size)
-	if n, err := fr.reader.Read(data); err != nil {
-		return nil, err
-	} else {
-		if uint32(n) < size {
-			_, err := fr.reader.Read(data[n:])
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return data, nil
 }
