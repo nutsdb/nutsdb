71fcfe2c0a60da0a52208bcc5bb2499e055edd7a
zhangjingchen <qq1346565062@outlook.com>
2023-07-23 11:28:57 +0800

    test:test for WithSyncEnable, WithMaxFdNumsInCache , WithCleanFdsCacheThreshold in options.go and ReadNode in bptree.go

--- options_test.go
+++ options_test.go
@@ -24,3 +24,39 @@ func TestWithRWMode(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, db.opt.RWMode, MMap)
 }
+
+func TestWithSyncEnable(t *testing.T) {
+	db, err = Open(DefaultOptions,
+		WithDir("/tmp/nutsdb"),
+		WithSyncEnable(false),
+	)
+
+	assert.NoError(t, err)
+	assert.False(t, db.opt.SyncEnable)
+
+	defer db.Close()
+}
+
+func TestWithMaxFdNumsInCache(t *testing.T) {
+	db, err = Open(DefaultOptions,
+		WithDir("/tmp/nutsdb"),
+		WithMaxFdNumsInCache(100),
+	)
+
+	assert.NoError(t, err)
+	assert.Equal(t, db.opt.MaxFdNumsInCache, 100)
+
+	defer db.Close()
+}
+
+func TestWithCleanFdsCacheThreshold(t *testing.T) {
+	db, err = Open(DefaultOptions,
+		WithDir("/tmp/nutsdb"),
+		WithCleanFdsCacheThreshold(0.5),
+	)
+
+	assert.NoError(t, err)
+	assert.Equal(t, db.opt.CleanFdsCacheThreshold, 0.5)
+
+	defer db.Close()
+}
