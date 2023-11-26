004b47e1ec205e96138e1f91b62c0404e6d01439
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2023-11-26 23:38:30 +0800

    fix the failue test cases

--- bucket_manager_test.go
+++ bucket_manager_test.go
@@ -89,8 +89,7 @@ func TestBucketManager_DeleteBucketIsolation(t *testing.T) {
 
 func txNewBucket(t *testing.T, db *DB, bucket string, ds uint16, expectErr error, finalExpectErr error) {
 	err := db.Update(func(tx *Tx) error {
-		success, err2 := tx.NewBucket(ds, bucket)
-		assert.Equal(t, true, success)
+		err2 := tx.NewBucket(ds, bucket)
 		assertErr(t, expectErr, err2)
 		return nil
 	})
