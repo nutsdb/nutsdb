cfd8c3bba69247b12ec4d1b3197e084f606d47d8
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2023-12-26 18:19:40 +0800

    rebuild the data structure of pending BTree and Pending Write Entires (#570)

--- pending.go
+++ pending.go
@@ -3,10 +3,41 @@ package nutsdb
 // pendingBucketList the uncommitted bucket changes in this Tx
 type pendingBucketList map[Ds]map[BucketName]*Bucket
 
+// pendingEntriesInBTree means the changes Entries in DataStructureBTree in the Tx
+type pendingEntriesInBTree map[BucketName]map[string]*Entry
+
 // pendingEntryList the uncommitted Entry changes in this Tx
 type pendingEntryList struct {
-	entries map[Ds]map[BucketName][]*Entry
-	size    int
+	entriesInBTree pendingEntriesInBTree
+	entries        map[Ds]map[BucketName][]*Entry
+	size           int
+}
+
+func NewPendingEntriesList() *pendingEntryList {
+	pending := &pendingEntryList{
+		entriesInBTree: map[BucketName]map[string]*Entry{},
+		entries:        map[Ds]map[BucketName][]*Entry{},
+		size:           0,
+	}
+	return pending
+}
+
+func (pending *pendingEntryList) submitEntry(ds Ds, bucket string, e *Entry) {
+	switch ds {
+	case DataStructureBTree:
+		if _, exist := pending.entriesInBTree[bucket]; !exist {
+			pending.entriesInBTree[bucket] = map[string]*Entry{}
+		}
+		pending.entriesInBTree[bucket][string(e.Key)] = e
+	default:
+		if _, exist := pending.entries[ds]; !exist {
+			pending.entries[ds] = map[BucketName][]*Entry{}
+		}
+		entries := pending.entries[ds][bucket]
+		entries = append(entries, e)
+		pending.entries[ds][bucket] = entries
+	}
+	pending.size++
 }
 
 // rangeBucket input a range handler function f and call it with every bucket in pendingBucketList
@@ -23,10 +54,16 @@ func (p pendingBucketList) rangeBucket(f func(bucket *Bucket) error) error {
 }
 
 // toList collect all the entries in pendingEntryList to a list.
-func (ens *pendingEntryList) toList() []*Entry {
-	list := make([]*Entry, ens.size)
+func (pending *pendingEntryList) toList() []*Entry {
+	list := make([]*Entry, pending.size)
 	var i int
-	for _, entriesInDS := range ens.entries {
+	for _, entriesInBucket := range pending.entriesInBTree {
+		for _, entry := range entriesInBucket {
+			list[i] = entry
+			i++
+		}
+	}
+	for _, entriesInDS := range pending.entries {
 		for _, entries := range entriesInDS {
 			for _, entry := range entries {
 				list[i] = entry
