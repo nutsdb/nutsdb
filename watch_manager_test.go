f71625be7b7dac8fd51cd9bf5b4df2b9fa1f38df
bietrokhovandi212 <winqti@gmail.com>
2025-11-08 11:04:26 +0000

    feat: distribute batch of batch of messasges

--- watch_manager_test.go
+++ watch_manager_test.go
@@ -2,7 +2,6 @@ package nutsdb
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -10,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSubscribeAndSendMessage(t *testing.T) {
+func TestWatchManager_SubscribeAndSendMessage(t *testing.T) {
 
 	t.Run("subscribe and send message", func(t *testing.T) {
 		wm := startDistributor()
@@ -33,72 +32,6 @@ func TestSubscribeAndSendMessage(t *testing.T) {
 		case <-time.After(1 * time.Second):
 			t.Fatal("timeout waiting for message")
 		}
-	})
-
-	t.Run("Multiple subscribers", func(t *testing.T) {
-		wm := startDistributor()
-		defer wm.close()
-
-		wg := sync.WaitGroup{}
-		expectedMsgCount := 10 // Each subscriber expects 10 messages
-
-		// Start 100 subscriber goroutines
-		for i := 0; i < 100; i++ {
-			wg.Add(1)
-			go func(i int) {
-				defer wg.Done()
-				bucket := fmt.Sprintf("bucket_test_%d", i)
-				key := fmt.Sprintf("key_test_%d", i)
-
-				receiveChan, err := wm.subscribe(bucket, key)
-				assert.NoError(t, err)
-
-				receivedCount := 0
-				timeout := time.After(10 * time.Second)
-
-				for receivedCount < expectedMsgCount {
-					select {
-					case message, ok := <-receiveChan:
-						if !ok {
-							// Channel closed
-							return
-						}
-
-						assert.Equal(t, bucket, message.bucket)
-						assert.Equal(t, key, message.key)
-						assert.NotNil(t, message.value)
-
-						receivedCount++
-
-					case <-timeout:
-						t.Errorf("Subscriber %d only received %d/%d messages",
-							i, receivedCount, expectedMsgCount)
-						return
-					}
-				}
-			}(i)
-		}
-
-		// Give subscribers time to register
-		time.Sleep(100 * time.Millisecond)
-
-		// Send 1000 messages (10 per subscriber on average)
-		for i := 0; i < 1000; i++ {
-			index := rand.Intn(100)
-			bucket := fmt.Sprintf("bucket_test_%d", index)
-			key := fmt.Sprintf("key_test_%d", index)
-			value := fmt.Sprintf("value_test_%d_%d", index, i)
-
-			err := wm.sendMessage(&message{
-				bucket: bucket,
-				key:    key,
-				value:  []byte(value),
-			})
-			assert.NoError(t, err)
-		}
-
-		// Wait for all subscribers to receive their messages
-		wg.Wait()
 	})
 
 	t.Run("subscribe and drop messages", func(t *testing.T) {
@@ -129,6 +62,9 @@ func TestSubscribeAndSendMessage(t *testing.T) {
 		for i := 0; i < messagesToSend; i++ {
 			value := fmt.Sprintf("value_test_%d", i)
 			err := wm.sendMessage(&message{bucket: bucket, key: key, value: []byte(value)})
+			if err != nil {
+				fmt.Printf("[watch_manager_test] Send message err %d: %v\n", i, err)
+			}
 			assert.NoError(t, err)
 		}
 
@@ -155,6 +91,74 @@ func TestSubscribeAndSendMessage(t *testing.T) {
 		// Now verify subscriber was removed
 		assert.Equal(t, drained, 1000)
 		assert.NotContains(t, wm.lookup, bucket)
+	})
+
+	t.Run("multiple subscribers", func(t *testing.T) {
+		wm := startDistributor()
+		defer wm.close()
+
+		numSubscribers := 100
+		messagesPerSubscriber := 10
+
+		wg := sync.WaitGroup{}
+
+		// Start 100 subscriber goroutines
+		for i := 0; i < numSubscribers; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				bucket := fmt.Sprintf("bucket_test_%d", i)
+				key := fmt.Sprintf("key_test_%d", i)
+				receivedCounts := 0
+
+				receiveChan, err := wm.subscribe(bucket, key)
+				assert.NoError(t, err)
+
+				timeout := time.After(5 * time.Second)
+
+				for {
+					select {
+					case message, ok := <-receiveChan:
+						if !ok {
+							break
+						}
+
+						assert.Equal(t, bucket, message.bucket)
+						assert.Equal(t, key, message.key)
+						assert.NotNil(t, message.value)
+
+						receivedCounts++
+
+					case <-timeout:
+						goto done
+					}
+				}
+			done:
+				assert.Equal(t, messagesPerSubscriber, receivedCounts)
+			}(i)
+		}
+
+		// Give subscribers time to register
+		time.Sleep(100 * time.Millisecond)
+
+		// Send exactly 10 messages to each subscriber (deterministic)
+		for i := 0; i < numSubscribers; i++ {
+			for j := 0; j < messagesPerSubscriber; j++ {
+				bucket := fmt.Sprintf("bucket_test_%d", i)
+				key := fmt.Sprintf("key_test_%d", i)
+				value := fmt.Sprintf("value_test_%d_%d", i, j)
+
+				err := wm.sendMessage(&message{
+					bucket: bucket,
+					key:    key,
+					value:  []byte(value),
+				})
+				assert.NoError(t, err)
+			}
+		}
+
+		// Wait for all subscribers to finish (with timeout)
+		wg.Wait()
 	})
 }
 
