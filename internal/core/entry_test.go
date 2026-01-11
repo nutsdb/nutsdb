2fc135d03e3f913ea9da6619974607a62d7815d1
dxyinme <dxyinme@outlook.com>
2026-01-11 12:52:48 +0800

    update

--- internal/core/entry_test.go
+++ internal/core/entry_test.go
@@ -15,10 +15,13 @@
 package core_test
 
 import (
+	"encoding/binary"
+	"hash/crc32"
 	"testing"
 
 	"github.com/nutsdb/nutsdb/internal/core"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/xujiajun/utils/strconv2"
 )
 
@@ -49,4 +52,100 @@ func Test_EntryCreation(t *testing.T) {
 	assert.Equal(t, entry.Key, k)
 	assert.Equal(t, entry.Value, v)
 	assert.True(t, entry.IsBelongsToBTree())
+}
+
+func Test_EntryEncode(t *testing.T) {
+	r := require.New(t)
+	k := []byte("key")
+	v := []byte("value")
+	ttl := 12
+	txId := 121212
+	bucketId := uint64(9)
+	entry := core.NewEntry().
+		WithKey(k).
+		WithValue(v).
+		WithMeta(
+			core.NewMetaData().
+				WithTTL(uint32(ttl)).
+				WithTxID(uint64(txId)).
+				WithDs(core.DataStructureBTree).
+				WithBucketId(bucketId).
+				WithKeySize(3).
+				WithValueSize(5),
+		)
+
+	r.Equal(entry.Key, k)
+	data := entry.Encode()
+
+	checkFuncs := []func(buf []byte) []byte{
+		func(buf []byte) []byte {
+			// crc32 check
+			t.Log("crc32 check")
+			length := 4
+			crc := binary.LittleEndian.Uint32(buf[0:length])
+			r.Equal(crc, crc32.ChecksumIEEE(buf[length:]))
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			// timestamp check
+			t.Log("timestamp check")
+			ts, length := binary.Uvarint(buf[0:])
+			r.Equal(entry.Meta.Timestamp, ts)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			// key size check
+			t.Log("keySize check")
+			ks, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.KeySize), ks)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("valueSize check")
+			vs, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.ValueSize), vs)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("Flag check")
+			flag, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.Flag), flag)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("ttl check")
+			ttl, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.TTL), ttl)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("status check")
+			status, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.Status), status)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("ds check")
+			ds, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.Ds), ds)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("txId check")
+			txId, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.TxID), txId)
+			return buf[length:]
+		},
+		func(buf []byte) []byte {
+			t.Log("bucketId check")
+			txId, length := binary.Uvarint(buf[0:])
+			r.Equal(uint64(entry.Meta.BucketId), txId)
+			return buf[length:]
+		},
+	}
+
+	for _, f := range checkFuncs {
+		data = f(data)
+	}
+	r.Equal("keyvalue", string(data))
 }
